1e291fad393e26a6a86e2a3048bc0b6811421795
clatterrr <40931228+clatterrr@users.noreply.github.com>
2020-10-03 15:05:05 +0800

    LBM 1D diffusion equation D1Q3

--- LBM with Computer Codes Chapter5.m
+++ LBM with Computer Codes Chapter5.m
@@ -1,8 +1,16 @@
-% LBM 1D diffusion equation D1Q2
+% LBM 1D diffusion equation D1Q3
 clear
 m = 101;
 dx = 1.0;
 rho = zeros(m);%这玩意就是温度T
+w0 = 4./6.;
+w1 = 1./6.;
+w2 = w1;
+c2 = 1./3.;
+
+alpha = 0.25;
+omega = 1/(3 * alpha + 0.5);
+f0 = zeros(m);
 f1 = zeros(m);
 flux = zeros(m);
 x = zeros(m);
@@ -12,12 +20,11 @@ for i = 1:m-1
     x(i+1)=x(i)+dx;
 end
 
-alpha = 0.25;
-omega = 1/(alpha + 0.5);
 twall = 1.0;%左边界温度为1.0
-nstep = 2;
+nstep = 200;
 
 for i = 1:m
+    f0(i) = 0.5*rho(i);
     f1(i) = 0.5*rho(i);
     f2(i) = 0.5*rho(i);
 end
@@ -25,7 +32,9 @@ end
 %Collision
 for k1 = 1:nstep
     for i = 1:m
-        feq = 0.5 * rho(i);
+        feq0 = w0 * rho(i);
+        feq = w1 * rho(i);
+        f0(i) = (1-omega)*f1(i) + omega*feq0;
         f1(i) = (1-omega)*f1(i) + omega*feq;
         f2(i) = (1-omega)*f2(i) + omega*feq;
     end
@@ -36,20 +45,26 @@ for k1 = 1:nstep
         f2(i) = f2(i+1);    % f2(1) = f2(3)   , f2(2)   = f2(3)   ...  f2(m-1) = f2(m)
     end
     %Boundary condition
-    f1(1) = twall - f2(1);
+    f1(1) = twall - f2(1) - f0(1);
     f1(m) = f1(m-1);
     f2(m) = f2(m-1);
+    f0(m) = f0(m-1);
 
     for j = 1:m
-        rho(j) = f1(j) + f2(j);
+        rho(j) = f1(j) + f2(j) + f0(j);
     end
 end
 
 
 %Flux
+fluxq = zeros(m);
 for k = 1:m
-    flux(k) = omega*(f1(k) - f2(k));
+    flux(k) = omega*(f1(k) - f2(k))/c2;
 end
+for k = 1:m 
+    fluxq(k) = rho(k)-rho(k+1);
+end
+fluxq(m) = fluxq(m-1);
 
 figure(1)
 plot(x,rho)
@@ -58,7 +73,7 @@ plot(x,rho)
    ylabel('T')
 
 figure(2)
-plot(x,flux,'o')
+plot(x,flux,'o',x,fluxq,'x')
     title('Flux, time step = 200')
     xlabel('X')
     ylabel('Flux')
